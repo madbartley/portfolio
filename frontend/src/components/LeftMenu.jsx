32d6452a3f930aebe6254dde51d78b781526e657
Madeline Bartley <he9169@wayne.edu>
2025-04-17 16:06:42 -0400

    Added title and left menu

---
+++ frontend/src/components/LeftMenu.jsx
@@ -0,0 +1,15 @@
+import '../App.css'
+
+function LeftMenu() {
+
+    return(
+        <div>
+            <div className="left-menu">
+                <h2>Explore</h2>
+
+            </div>
+        </div>
+    )
+}
+
+export default LeftMenu;
---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,15 @@
+import '../App.css'
+
+function Title(props) {
+
+    return(
+        <div>
+            <div className="title-name">
+            <h1>Hello</h1>
+            <h1>My name is {props.name}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Title;
