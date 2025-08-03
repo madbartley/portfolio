bfe73fc027d1b05d11633a9dcb2f44b8a62a6c74
Madeline Bartley <madeline.bartley@outlook.com>
2025-08-03 12:55:56 -0400

    Refactored maxed-project to make everything a separate component

---
+++ frontend/src/components/Description.jsx
@@ -0,0 +1,15 @@
+import '../styles/MixedProject.css'
+import { useState, createContext } from "react";
+
+
+
+const Description = () => {
+
+    return (
+        <div>
+        <div id="project-title">Title</div><div id="description">This is where I will write about my project!</div>
+        </div>
+    )
+}
+
+export default Description;
