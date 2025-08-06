5a9cde9703697daacbd3239c54c4158456c8d7d2
Madeline Bartley <madeline.bartley@outlook.com>
2025-08-06 19:56:46 -0400

    Updated description text

--- frontend/src/components/Description.jsx
+++ frontend/src/components/Description.jsx
@@ -4,7 +4,7 @@ const Description = (props) => {
 
     return (
         <div className="inner-description">
-        <div id="project-title">README</div><div id="description">This is where I will write about my project!</div>
+        <div id="project-title">README</div><div id="description">This is where I will write my project description.</div>
         </div>
     )
 }
